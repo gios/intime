be94c3058d0a014e1ee1ede7e0174d7d9bc89dd5
gios <pblazhchuk@gmail.com>
2015-09-07 23:24:17 +0300

    universal react

---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import React from 'react';
+import Router from 'react-router';
+const app = express();
+
+// set up Jade
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+import routes from '../shared/routes';
+
+app.get('/*', function (req, res) {
+  Router.run(routes, req.url, Handler => {
+    let content = React.renderToString(<Handler />);
+    res.render('index', { content: content });
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening at http://localhost:3000');
+});
