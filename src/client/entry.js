4661ed0a9fd76876581bcbf1e04a82ec6c6d8991
gios <pblazhchuk@gmail.com>
2015-10-11 16:59:27 +0300

    react transform init

--- src/client/entry.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Router from "react-router";
-import routes from "../shared/routes";
-
-Router.run(routes, Router.HistoryLocation, (Handler) => {
-  React.render(<Handler/>, document.getElementById("react-app"));
-});
--- src/shared/components/AppController.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class AppController extends React.Component {
-  render() {
-    return <div>Hello Universal</div>;
-  }
-}
