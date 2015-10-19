dab0353c2002337f2f50834166c8f2b1fa09288b
gios <pblazhchuk@gmail.com>
2015-10-19 22:56:45 +0300

    Implement history for react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link } from 'react-router';
+import history from './history';
 import { Login } from './login/Login.js';
 import { Registration } from './registration/Registration.js';
 import { NoMatch } from './NoMatch.js';
@@ -10,7 +11,7 @@ import '../node_modules/font-awesome/scss/font-awesome.scss';
 import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <Route path='/' component={Login}>
       <Route path='login' component={Login} />
     </Route>
--- src/login/Login.js
+++ src/login/Login.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import Parse from 'parse';
 import { Footer } from '../footer/Footer.js';
+import history from '../history.js';
 
 export class Login extends Component {
   constructor(props) {
@@ -8,13 +11,24 @@ export class Login extends Component {
     this.items = [];
   }
 
+  componentWillMount() {
+    let user = new Parse.User();
+    // history.replaceState(null, '/some/path')
+  }
+
+  signUp() {
+    let username = this.refs.username.value;
+    let password = this.refs.password.value;
+    console.log(username, password);
+  }
+
   render() {
     return (
       <div className='login-block'>
         <h1>Login</h1>
-        <span className='user-icon'></span><input type='text' placeholder='Username' id='username' />
-        <span className='password-icon'></span><input type='password' placeholder='Password' id='password' />
-        <button>Submit</button>
+        <span className='user-icon'></span><input type='text' placeholder='Username' ref='username' />
+        <span className='password-icon'></span><input type='password' placeholder='Password' ref='password' />
+        <button onClick={_.bind(this.signUp, this)}>Submit</button>
         <Footer />
       </div>
     );
