b92367a5b18ef93ab346e75e38ec6fbfb73afe4f
gios <pblazhchuk@gmail.com>
2015-10-19 11:02:45 +0300

    Integrate bootstrap and refactor project structure

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link } from 'react-router';
 import { Login } from './login/Login.js';
+import { Registration } from './registration/Registration.js';
 import { NoMatch } from './NoMatch.js';
 
 import './styles/base.scss';
 import '../node_modules/font-awesome/scss/font-awesome.scss';
+import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 
 ReactDOM.render(
   <Router>
-    <Route path="/" component={Login}>
-      <Route path="login" component={Login} />
+    <Route path='/' component={Login}>
+      <Route path='login' component={Login} />
     </Route>
-    <Route path="*" component={NoMatch}/>
+    <Route path='registration' component={Registration} />
+    <Route path='*' component={NoMatch}/>
   </Router>
 , document.getElementById('intime'));
--- src/registration/Registration.js
+++ src/registration/Registration.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { VERSION, CURRENT_YEAR } from '../constants.js';
 
-export class Login extends Component {
+export class Registration extends Component {
   constructor(props) {
     super(props);
-    this.state = { items: null };
-    this.items = [];
   }
 
   render() {
     return (
-      <div className='login-block'>
-        <h1>Login</h1>
+      <div className='registration-block'>
+        <h1>Registration</h1>
         <span className='user-icon'></span><input type='text' placeholder='Username' id='username' />
         <span className='password-icon'></span><input type='password' placeholder='Password' id='password' />
-        <button>Submit</button>
+        <span className='password-icon'></span><input type='password' placeholder='Re-Password' id='re-password' />
+        <button>Registration</button>
         <div className='footer'>
           <div className='info-footer'>
             &copy; {CURRENT_YEAR} Version {VERSION}
